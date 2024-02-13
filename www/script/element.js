d5ad809a6ccb0d0a89131619d9b3b07b1b6dcdbf
keishi <iotegn1763@gmail.com>
2024-02-14 08:39:57 +0900

    ADD: フォルダ構造を構築

---
+++ www/script/element.js
@@ -0,0 +1,223 @@
+/**
+ * 座標を管理するためのクラス
+ */
+class Position {
+
+    /**
+     * @constructor
+     * @param {number} x - X 座標
+     * @param {number} y - Y 座標
+     */
+    constructor(x, y){
+        this.x = x;
+        this.y = y;  
+    }
+
+    /**
+     * 値を設定する
+     * @param {number} [x] - 設定する X 座標
+     * @param {number} [y] - 設定する Y 座標
+     */
+    set(x, y){
+        if(x != null){this.x = x;}
+        if(y != null){this.y = y;}
+    }
+}
+
+/**
+ * 問題のブロックを管理するクラス
+ */
+class Block{
+    /**
+     * @constructor
+     * @param {CanvasRenderingContext2D} ctx - 描画などに利用する2Dコンテキスト
+     * @param {number} x - X 座標
+     * @param {number} y - Y 座標
+     * @param {number} radius - 半径
+     * @param {number} life - ライフ（生存フラグを兼ねる）
+     * @param {number} area - 解答可能エリア
+     * @param {string} color - ブロックの色 
+     * @param {Question} question - 計算問題を管理するクラス
+     */
+    constructor(ctx, x, y, radius, life, area, color = '#0000ff'){
+        this.ctx = ctx;
+        this.position = new Position(x, y);
+        this.radius = radius;
+        this.life = life;
+        this.area = area;
+        this.color = color;
+        this.speed = 1;
+        this.question = new Question();
+    }
+
+    /**
+     * ブロックを描画する
+     */
+    draw(){      
+        // パスの設定を開始することを明示する
+        this.ctx.beginPath();
+        // 円のパスを設定する
+        this.ctx.arc(
+            this.position.x,
+            this.position.y,
+            this.radius,
+            0.0,
+            Math.PI * 2.0
+        );
+        // 円の色を設定する
+        this.ctx.fillStyle = this.color;
+        // パスを閉じることを明示する
+        this.ctx.closePath();
+        // 設定したパスで円の描画を行う
+        this.ctx.fill();
+
+        // 問題のフォントを設定
+        this.ctx.fillStyle = this.question.color;
+        this.ctx.font = this.question.font;
+        this.ctx.textAlign = "center";
+        //　テキストを表示
+        this.ctx.fillText(
+            `${this.question.number1} ${this.question.operator} ${this.question.number2}`,
+            this.position.x,
+            this.position.y + 5  // 数字が円の中心にくるよう微調整
+        ); 
+    }
+
+    /**
+     * ブロックの位置の更新
+     */
+    update(){
+        // ブロックのライフが0以下(非生存)の場合何もしない
+        if(this.life <= 0){return;}
+
+        // 下に進める(y座標を進める)
+        this.position.y += this.speed;
+
+        // 解答可能エリア外に移動したらライフ0(非生存)に設定する
+        if(this.position.y + this.radius > this.area){
+            this.life = 0;
+        }
+
+        // 円の描画
+        this.draw();
+    }
+}
+
+/**
+ * 計算問題を管理するクラス
+ */
+class Question {
+    /**
+     * @constructor
+     * @param {string} color - 文字の色
+     * @param {string} font - 文字のフォント
+     */
+    constructor(color = '#ffffff', font = '20px Arial') {
+        this.operator = this.getRandomOperator();
+        this.number1 = this.getRandomNumber();
+        this.number2 = this.getRandomNumber();
+        this.answer = this.calculateAnswer();
+        this.color = color;
+        this.font = font
+    }
+
+    /**
+     * ランダムな四則演算子を返す
+     * @returns {string} - 四則演算子
+     */
+    getRandomOperator() {
+        const operators = ['+', '-', '×', '÷'];
+        return operators[Math.floor(Math.random() * operators.length)];
+    }
+
+    /**
+     * ランダムな2桁以内の数字を返す
+     * @returns {number} - ランダムな数字
+     */
+    getRandomNumber() {
+        return Math.floor(Math.random() * 90) + 10; // 10以上99以下の乱数を生成
+    }
+
+    /**
+     * 与えられた演算子に基づいて計算結果を返す
+     * @returns {number} - 計算結果
+     */
+    calculateAnswer() {
+        let result;
+        switch (this.operator) {
+            case '+':
+                result = this.number1 + this.number2;
+                break;
+            case '-':
+                result = this.number1 - this.number2;
+                break;
+            case '×':
+                result = this.number1 * this.number2;
+                break;
+            case '÷':
+                // とりあえず切り捨ての結果
+                result = Math.floor(this.number1 / this.number2);
+                break;
+            default:
+                throw new Error("Invalid operator");
+        }
+        return result;
+    }
+}
+
+/**
+ * 数字キーを管理するクラス
+ */
+class NumberKey{
+    /**
+     * @constructor
+     * @param {CanvasRenderingContext2D} ctx - 描画などに利用する2Dコンテキスト
+     * @param {string} type - キーのタイプ
+     * @param {number} x - X座標
+     * @param {number} y - Y座標
+     * @param {number} radiusX - 楕円の長辺半径
+     * @param {number} radiusY - 楕円の短辺半径
+     * @param {number} rotation - 楕円の回転角度(radian)
+     * @param {string} color - 楕円の色
+     * @param {string} fontColor - 文字の色
+     * @param {string} font - 文字のフォント
+     */
+    constructor(ctx, type, x, y, radiusX, radiusY, rotation = 0.0, color = '#ffffff', fontColor = '#000000', font = '20px Arial') {
+        this.ctx = ctx;
+        this.type = type;
+        this.position = new Position(x,y);
+        this.radiusX = radiusX;
+        this.radiusY = radiusY;
+        this.rotation = rotation;
+        this.color = color;
+        this.fontColor = fontColor;
+        this.font = font;
+    }
+
+    /**
+     * 数字キーの更新
+     */
+    update(){
+        // パスの設定を開始することを明示する
+        this.ctx.beginPath();
+        // 楕円のパスを設定する
+        this.ctx.ellipse(this.position.x, this.position.y, this.radiusX, this.radiusY, this.rotation, 0, 2 * Math.PI);
+        //楕円の色を設定する
+        this.ctx.fillStyle = this.color;
+        // パスを閉じることを明示する
+        this.ctx.closePath();
+        // 設定したパスで楕円の描画を行う
+        this.ctx.fill();
+
+        // 数字のフォントを設定
+        this.ctx.fillStyle = this.fontColor;
+        this.ctx.font = this.font;
+        this.ctx.textAlign = "center";
+        //　テキストを表示
+        this.ctx.fillText(
+            `${this.type}`,
+            this.position.x,
+            this.position.y + 5  // 数字が円の中心にくるよう微調整
+        ); 
+    }
+}
---
+++ www/script/script.js
@@ -0,0 +1,215 @@
+
+(() => {
+    /**
+     * canvas の幅
+     * @type {number}
+     */
+    const CANVAS_WIDTH = 400;
+    /**
+     * canvas の高さ
+     * @type {number}
+     */
+    const CANVAS_HEIGHT = 600;
+
+    /**
+     * Canvas2D API をラップしたユーティリティクラス
+     * @type {Canvas2DUtility}
+     */
+    let util = null;
+    /**
+     * 描画対象となる Canvas Element
+     * @type {HTMLCanvasElement}
+     */
+    let canvas = null;
+    /**
+     * Canvas2D API のコンテキスト
+     * @type {CanvasRenderingContext2D}
+     */
+    let ctx = null;
+    /**
+     * 数字キーの高さ
+     * @type {number}
+     */
+    const KEYPAD_HEIGHT = 200;
+    /**
+     * 問題の最大個数
+     * @param {number}
+     */
+    const BLOCK_MAX_COUNT = 3;
+    /**
+     * ブロックのインスタンスを格納する配列
+     * @type {Array<Brock>}
+     */
+    let blockArray = [];
+    /**
+     * 数字キーのインスタンスを格納する配列
+     * @type {Array<NumberKey>}
+     */
+    let numberKeyArray = [];
+    /**
+     * 数字キーの初期化のための座標を格納した2次元配列
+     * (0~9,c,-,.,enter)[X座標,Y座標,X軸半径,Y軸半径]
+     * @type {Array<number>}
+     */
+    let numberKeyCoordinateArray = [
+        [113, 565, 80, 20],
+        [70, 520, 40, 20],
+        [155, 520, 40, 20],
+        [240, 520, 40, 20],
+        [70, 475, 40, 20],
+        [155, 475, 40, 20],
+        [240, 475, 40, 20],
+        [70, 430, 40, 20],
+        [155, 430, 40, 20],
+        [240, 430, 40, 20],
+        [240, 565, 40, 20],
+        [335, 430, 40, 20],
+        [335, 475, 40, 20],
+        [335, 543, 40, 40]
+    ];
+
+
+    /**
+     * ページのロードが完了したときに発火する load イベント
+     */
+    window.addEventListener('load', () => {
+        // ユーティリティクラスを初期化
+        util = new Canvas2DUtility(document.body.querySelector('#main_canvas'));
+        // ユーティリティクラスから canvas を取得
+        canvas = util.canvas;
+        // ユーティリティクラスから 2d コンテキストを取得
+        ctx = util.context;
+
+        // 初期化処理を行う
+        initialize();
+        // 描画処理を行う
+        render();
+
+        // クリックイベントとタッチイベントを追加
+        canvas.addEventListener('click', ClickOrTouch);
+        canvas.addEventListener('touchstart', ClickOrTouch);
+
+
+    }, false);
+
+    /**
+     * canvas やコンテキストを初期化する
+     */
+    function initialize(){
+        // canvas の大きさを設定
+        canvas.width = CANVAS_WIDTH;
+        canvas.height = CANVAS_HEIGHT;
+
+        // ブロックを初期化する
+        for(let i = 0; i < BLOCK_MAX_COUNT; ++i){
+            blockArray[i] = new Block(ctx, 75 + 125 * i, -50, 50, 1, canvas.height - KEYPAD_HEIGHT);
+        }
+
+        // 数字キーの初期化
+        initializeNumberKey();
+    }
+
+    /**
+     * 描画処理を行う
+     */
+    function render(){
+        // 描画前に画面全体を白で塗りつぶす
+        util.drawRect(0, 0, canvas.width, canvas.height, '#ffffff');
+        // 数字キーのエリアの描画
+        util.drawRect(0, canvas.height - KEYPAD_HEIGHT, canvas.width, KEYPAD_HEIGHT, '#ff0000');
+        // ブロックの更新
+        blockArray.map((v) => {
+            v.update();
+        });
+        // 数字キーの更新
+        numberKeyArray.map((v) => {
+            v.update();
+        });
+
+        // フレーム更新ごとに再起呼び出し
+        requestAnimationFrame(render);
+    }
+
+    function initializeNumberKey(){
+        for(let i = 0; i < 10; ++i){
+            numberKeyArray[i] = new NumberKey(
+                ctx,
+                String(i),
+                numberKeyCoordinateArray[i][0],
+                numberKeyCoordinateArray[i][1],
+                numberKeyCoordinateArray[i][2],
+                numberKeyCoordinateArray[i][3]
+                );
+        }
+        numberKeyArray[10] = new NumberKey(
+            ctx,
+            'C',
+            numberKeyCoordinateArray[10][0],
+            numberKeyCoordinateArray[10][1],
+            numberKeyCoordinateArray[10][2],
+            numberKeyCoordinateArray[10][3]
+        );
+        numberKeyArray[11] = new NumberKey(
+            ctx,
+            '-',
+            numberKeyCoordinateArray[11][0],
+            numberKeyCoordinateArray[11][1],
+            numberKeyCoordinateArray[11][2],
+            numberKeyCoordinateArray[11][3]
+        );
+        numberKeyArray[12] = new NumberKey(
+            ctx,
+            '.',
+            numberKeyCoordinateArray[12][0],
+            numberKeyCoordinateArray[12][1],
+            numberKeyCoordinateArray[12][2],
+            numberKeyCoordinateArray[12][3]
+        );
+        numberKeyArray[13] = new NumberKey(
+            ctx,
+            'enter',
+            numberKeyCoordinateArray[13][0],
+            numberKeyCoordinateArray[13][1],
+            numberKeyCoordinateArray[13][2],
+            numberKeyCoordinateArray[13][3]
+        );
+    }
+
+    /**
+     * クリックまたはタッチされた時の処理
+     * @param {MouseEvent|TouchEvent} event クリックまたはタッチイベント
+     */
+    function ClickOrTouch(event) {
+        let x, y;
+
+        if(event.type === 'click'){
+            x = event.pageX - canvas.offsetLeft;
+            y = event.pageY - canvas.offsetTop;
+        }
+        else if(event.type === 'touchstart'){
+            let touch = event.touches[0];
+            x = touch.pageX - canvas.offsetLeft;
+            y = touch.pageY - canvas.offsetTop;
+        }
+
+        // if(x > 0 && x < CANVAS_WIDTH && y > 0 && y < CANVAS_HEIGHT - KEYPAD_HEIGHT){
+
+        // }
+        // else if(x > 0 && x < CANVAS_WIDTH && y > CANVAS_HEIGHT - KEYPAD_HEIGHT && y < CANVAS_HEIGHT){
+
+        // }
+    }
+
+    // /**
+    //  * 
+    //  * @param {number} x - クリックされたX座標 
+    //  * @param {number} y - クリックされたY座標
+    //  */
+    // function ClickQuestionArea(x,y){
+
+    // }
+
+    // function ClickKyeArea(x,y){
+
+    // }
+})();
