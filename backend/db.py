d1301d4ccedd21f23e2368c2d5d978424b6809d4
keishi <iotegn1763@gmail.com>
2024-02-15 13:50:16 +0900

    Update : db.pyででmongoDBとの連携をできるようにした 現状以下のように動作している localhost:5001/get_questionsにおいてDBから内容を取得し，表示させる 日本語表記になっていないのはいまいちわからないが，内容は正しいらしい
    db.pyではtxtファイルに保存された内容をquestion:answerの形式での形式でDBに保存

--- backend/db.py
+++ backend/db.py
@@ -1,30 +1,50 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
 
-host_name = "mongo"       # コンテナ名を指定
-port_num  = 27017
-user_name = "py_user"     # 作成ユーザーの名前
-user_pwd  = "py_pwd"      # 作成ユーザーのパスワード
-db_name   = "sample-db"   # 対象データベース
+QUESTION_FILE_PATH = "questions.txt"
 
-# スーパーユーザーでアクセス
-client = MongoClient(
-    host = host_name,
-    port = port_num,
-    username = "root",      # ここがポイント1個目!
-    password = "password",
-)
+def main():
+  host_name = "mongo"       # コンテナ名を指定
+  port_num  = 27017
+  user_name = "py_user"     # 作成ユーザーの名前
+  user_pwd  = "py_pwd"      # 作成ユーザーのパスワード
+  db_name   = "question-db"   # 対象データベース
 
-# 「sample-db」データベースにユーザーを追加
-db = client[db_name]
-db.command(                 # ここがポイント2個目!
-  'createUser', 
-  user_name,
-  pwd=user_pwd,
-  roles=['readWrite'],
-)
+  # スーパーユーザーでアクセス
+  client = MongoClient(
+      host = host_name,
+      port = port_num,
+      username = "root",      # ここがポイント1個目!
+      password = "password",
+  )
 
-# user確認
-for user in db.command('usersInfo').get('users'):
-    print(user)
+  db = client[db_name]
 
-client.close()
+  try:
+      # ユーザーを作成
+      db.command(
+          'createUser', 
+          user_name,
+          pwd=user_pwd,
+          roles=['readWrite'],
+      )
+      print(f"User {user_name} created.")
+  except errors.OperationFailure as e:
+      # ユーザーが既に存在する場合のエラーをキャッチ
+      print(e)
+  
+  questions_collection = db["questions"]
+  
+  # テキストファイルから問題を読み込む + DBに追加
+  with open(QUESTION_FILE_PATH, "r") as f:
+    for line in f:
+      question, answer = line.strip().split(",")
+      add_question(questions_collection, question, answer)
+      print(f"Added: {question} -> {answer}")
+
+
+# 問題を追加する関数
+def add_question(questions_collection, question, answer):
+    questions_collection.insert_one({'question': question, 'answer': answer})
+
+if __name__ == "__main__":
+    main()
